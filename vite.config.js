8228ee6adb91005f2b338ee8ae65064100af7697
Aaron Weiss <aaronwe@gmail.com>
2026-03-04 13:42:16 -0700

    Update vite base url to match censored-park-map repo

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/censored-parks-map/',
+  base: '/censored-park-map/',
   test: {
     environment: 'jsdom',
     globals: true,
